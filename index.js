5f55e1d9ce062e634832067c086639fbc18d51be
iamdevonbutler <jpescione@gmail.com>
2017-03-08 18:38:34 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const request = require('request-promise');
 
 const frequency = process.env.frequency || 60*60*5;
-const sites = process.env.sites ? process.env.sites.split(',') : null;
+const sites = process.env.sites ? process.env.sites.split(' ') : null;
 const pinbotDomain = process.env.pingbotDomain;
 
 if (!pinbotDomain) {
