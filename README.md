815ac3fad20fedb04e455b7779e06789fd387f9d
iamdevonbutler <jpescione@gmail.com>
2017-03-08 18:37:49 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # pingbot
+
 Pingbot for heroku
+
+## Setup
+
+Create 3 environment variables:
+
+* frequency - how often to ping (in seconds)
+* sites - domains of sites to ping (separate domains w/ a space " ")
+* pinbotDomain - domain pinbot is hosted on
---
+++ index.js
@@ -0,0 +1,22 @@
+const request = require('request-promise');
+
+const frequency = process.env.frequency || 60*60*5;
+const sites = process.env.sites ? process.env.sites.split(',') : null;
+const pinbotDomain = process.env.pingbotDomain;
+
+if (!pinbotDomain) {
+  throw `Enviornment vairable ${pinbotDomain} required`;
+}
+
+if (!sites) {
+  throw `Enviornment vairable ${sites} required`;
+}
+
+function ping() {
+  request(pinbotDomain).catch(console.log);
+  sites.forEach(site => {
+    request(site).catch(console.log);
+  });
+}
+
+setInterval(ping, frequency);
